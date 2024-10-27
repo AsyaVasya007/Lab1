55e31851c6c6f9d61b489f6cb4e36a1d9419c685
Anastasia Lesnikova <vassilisa789@gmail.com>
2024-10-27 19:34:54 +0300

    2nd commit: methods add, size, grow and constructors

--- src/main/java/main/Box.java
+++ src/main/java/main/Box.java
@@ -1,4 +1,6 @@
 package main;
 
 public interface Box<E> {
+    public int size();
+    public boolean add(E e);
 }
--- src/main/java/main/JavaArrayBox.java
+++ src/main/java/main/JavaArrayBox.java
@@ -6,4 +6,40 @@ public class JavaArrayBox<E> implements Box<E> {
     private Object[] array;
     private int size;
     private static final int DEFAULT_CAPACITY = 10;
+
+    public JavaArrayBox() {
+        this.array = new Object[DEFAULT_CAPACITY];
+    }
+
+    public JavaArrayBox(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Данный индекс не корректен: " + capacity);
+        } else {
+            this.array = new Object[capacity];
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    private Object[] grow() {
+        Object[] newArray = new Object[(int) (array.length * 1.5 + 1)];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        return array;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (size == array.length) {
+            grow();
+        }
+        array[size] = e;
+        size++;
+        return true;
+    }
 }
